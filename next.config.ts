53ec8c21cdc380d31826247b23129d62ca8354da
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-03 23:52:07 -0400

    Product display UI and additional styles
    Added a little mock-up of how the UI will look

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [new URL('https://schylling.com/wp-content/uploads/2024/08/DRDND_Image1.jpg')],
+  },
+}
+
 export default nextConfig;

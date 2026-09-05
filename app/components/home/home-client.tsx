459e8f5224fc3fc6e13e37a749039b65f48edc67
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-05 15:29:37 -0400

    Added search routing and half its functionality

--- app/components/home/home-client.tsx
+++ app/components/home/home-client.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+"use client"
+
+import { useState } from 'react'
 import SearchForm from './search-form';
 import Products from './products';
+import { searchProducts } from '@/utils/db-actions';
+import { Product } from '@/utils/interfaces';
+import { useRouter } from 'next/navigation';
 
 export default function HomePageClient() {
+  const router = useRouter()
+  const [searchProduct, setSearchProduct] = useState<string>("");
+
+  const goSearch = async() => {
+    if(searchProduct.trim() === "") return;
+
+    router.push(`/search?q=${encodeURIComponent(searchProduct)}`);
+    setSearchProduct("");
+  }
+
   return (
     <div className="flex flex-col items-center">
-      <SearchForm/>
+      <SearchForm searchProduct={searchProduct} setSearchProduct={setSearchProduct} goSearch={goSearch}/>
 
       <Products/>
     </div>
--- app/components/home/search-form.tsx
+++ app/components/home/search-form.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
+interface Props {
+  searchProduct: string;
+  setSearchProduct: React.Dispatch<React.SetStateAction<string>>;
+  goSearch: () => void
+}
 
-export default function SearchForm() {
+export default function SearchForm({searchProduct, setSearchProduct, goSearch}: Props) {
+
   return (
-    <form className="flex mt-3 gap-2">
-      <input type="text" className="border-2 border-[#ddd] rounded-md p-1" placeholder="Search"/>
+    <form className="flex mt-3 gap-2" onSubmit={goSearch}>
+      <input
+        type="text"
+        className="border-2 border-[#ddd] rounded-md p-1"
+        placeholder="Search"
+        value={searchProduct}
+        onChange={(e) => {
+          setSearchProduct(e.target.value);
+        }}
+      />
 
       <button className="border-2 border-[#ddd] bg-[#e7e7e7] duration-400 hover:bg-white rounded-md px-2">
         <FaMagnifyingGlass></FaMagnifyingGlass>

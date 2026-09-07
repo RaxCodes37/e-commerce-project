067493ec30176a78b85a36e5633473e90ca3a61c
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-07 16:15:13 -0400

    Finished cart design

--- app/components/cart/cart-client.tsx
+++ app/components/cart/cart-client.tsx
@@ -5,35 +5,45 @@ import CartProductsDisplay from "./cart-products";
 import { useEffect, useState } from "react";
 import { getProductsOnCart, removeFromCart } from "@/utils/db-actions";
 import e from "cors";
+import UserStuff from "../home/user-stuff";
+import BackButton from "../back-button";
 
 interface Props {
-  userId: string
+  userId: string;
 }
 
-export default function CartPageClient({userId}: Props) {
+export default function CartPageClient({ userId }: Props) {
   const [products, setProducts] = useState<ProductOnCart[]>([]);
 
   useEffect(() => {
-    const getProductsOnCartFunction = async() => {
+    const getProductsOnCartFunction = async () => {
       setProducts(await getProductsOnCart(userId));
-    }
+    };
 
     getProductsOnCartFunction();
   }, []);
 
-  const removeFromCartFunction = async(cartId: string) => {  
-    setProducts(products.filter(product => product.cartId !== cartId));
+  const removeFromCartFunction = async (cartId: string) => {
+    setProducts(products.filter((product) => product.cartId !== cartId));
 
     try {
       await removeFromCart(cartId);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
     <div>
-      <CartProductsDisplay products={products} removeFromCartFunction={removeFromCartFunction}/>
+      <div className="relative top-3">
+        <BackButton />
+        <UserStuff />
+      </div>
+
+      <CartProductsDisplay
+        products={products}
+        removeFromCartFunction={removeFromCartFunction}
+      />
     </div>
-  )
+  );
 }

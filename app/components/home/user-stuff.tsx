a2f22d05c2d6f05128500bf9e10af1ffbaf14326
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-07 16:35:35 -0400

    Fixed error

--- app/components/home/user-stuff.tsx
+++ app/components/home/user-stuff.tsx
@@ -8,7 +8,7 @@ export default function UserStuff() {
   const router = useRouter();
 
   return (
-    <div className="absolute left-4 top-9 flex flex-col sm:left-[85%] sm:flex-row sm:py-1 sm:text-2xl sm:top-0 xl:left-[90%] rounded-md bg-[#e7e7e7]">
+    <div className="absolute left-4 top-9 flex flex-col sm:left-[85%] sm:flex-row sm:py-1 sm:text-2xl sm:top-3 xl:left-[90%] rounded-md bg-[#e7e7e7]">
       <button className="duration-400 hover:bg-[#bcb7b7] rounded-l-md p-1 px-1.5 text-xl flex justify-center" onClick={() => router.push("/cart")}>
         <IoCart />
       </button>

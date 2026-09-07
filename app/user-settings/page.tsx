23dec37d69b493fe5914d9812c0f5a06cb7cfc88
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-06 23:29:11 -0400

    improved responsiveness and added missing button

--- app/user-settings/page.tsx
+++ app/user-settings/page.tsx
@@ -1,8 +1,10 @@
 import SignOutButton from "../components/auth/sign-out-button";
+import BackButton from "../components/back-button";
 
 export default function UserSettings() {
   return (
     <div className="flex justify-center">
+      <BackButton />
       <div className="mt-30 w-80 text-center" id="settings-div">
         <h2 className="text-2xl font-bold">Your settings</h2>
 

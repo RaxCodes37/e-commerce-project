844055d998dd1a466b6dc942fe9d8e69b016b744
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-06 22:41:52 -0400

    Refactored code

--- app/components/search/search-page.tsx
+++ app/components/search/search-page.tsx
@@ -1,27 +1,30 @@
-"use client"
+"use client";
 
 import { searchProducts } from "@/utils/db-actions";
 import { Product } from "@/utils/interfaces";
 import { useEffect, useState } from "react";
+import UserStuff from "../home/user-stuff";
+import IndividualProduct from "../product/display-individual-product";
 
 interface Props {
-  searchedProduct: string
+  searchedProduct: string;
 }
 
-export default function SearchPageClient({searchedProduct}: Props) {
-  const [products, setProducts] = useState<Product[]>([])
-  
+export default function SearchPageClient({ searchedProduct }: Props) {
+  const [products, setProducts] = useState<Product[]>([]);
+
   useEffect(() => {
-    const searchFunction = async() => {
+    const searchFunction = async () => {
       setProducts(await searchProducts(searchedProduct));
-    }
+    };
 
     searchFunction();
-  }, [searchedProduct])
-  
+  }, [searchedProduct]);
+
   return (
     <div>
-      
+      <UserStuff />
+      <IndividualProduct product={products} />
     </div>
-  )
+  );
 }

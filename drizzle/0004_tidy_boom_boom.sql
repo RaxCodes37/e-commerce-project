104b3af4177ce8186d9bdf5b60ad10990adc3505
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-07 15:17:21 -0400

    Migrations

---
+++ drizzle/0004_tidy_boom_boom.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "cart" ALTER COLUMN "product_on_cart_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "cart" ALTER COLUMN "product_on_cart_name" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "cart" ALTER COLUMN "product_on_cart_description" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "cart" ALTER COLUMN "product_on_cart_price" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "cart" ALTER COLUMN "potential_buyer_id" SET NOT NULL;

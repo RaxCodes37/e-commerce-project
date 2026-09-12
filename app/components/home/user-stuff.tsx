b0edfe8937b9487f2d80a938b731de334b13c931
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-12 01:41:44 -0400

    Fixed styles and added better responsiveness

--- app/components/home/user-stuff.tsx
+++ app/components/home/user-stuff.tsx
@@ -8,11 +8,17 @@ export default function UserStuff() {
   const router = useRouter();
 
   return (
-    <div className="absolute left-4 top-9 flex flex-col sm:left-[85%] sm:flex-row sm:py-1 sm:text-2xl sm:top-3 xl:left-[90%] rounded-md bg-[#e7e7e7]">
-      <button className="duration-400 hover:bg-[#bcb7b7] rounded-l-md p-1 px-1.5 text-xl flex justify-center" onClick={() => router.push("/cart")}>
+    <div className="absolute left-4 top-3 flex flex-col sm:left-[85%] sm:flex-row sm:text-2xl sm:top-3 xl:left-[90%] rounded-md bg-[#e7e7e7]">
+      <button
+        className="duration-400 hover:bg-[#bcb7b7] rounded-t-md sm:rounded-l-md sm:rounded-t-none p-1.5 text-xl flex justify-center"
+        onClick={() => router.push("/cart")}
+      >
         <IoCart />
       </button>
-      <button className="duration-400 hover:bg-[#bcb7b7] rounded-r-md p-1 px-1.5 text-xl flex justify-center" onClick={() => router.push("/user-settings")}>
+      <button
+        className="duration-400 hover:bg-[#bcb7b7] rounded-b-md sm:rounded-r-md sm:rounded-b-none p-1.5 px-1.5 text-xl flex justify-center"
+        onClick={() => router.push("/user-settings")}
+      >
         <HiUserCircle />
       </button>
     </div>
